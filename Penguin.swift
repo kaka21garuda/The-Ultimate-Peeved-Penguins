77739facfebd268c3562fed1244dccebda274260
Buka Cakrawala <kaka10garuda@gmail.com>
2016-06-28 11:01:03 -0700

    *Create reusable core game objects *Setup physics properties and tweak values *Use a custom class for the scene

---
+++ Penguin.swift
@@ -0,0 +1,9 @@
+//
+//  Penguin.swift
+//  The Ultimate Peeved Penguins
+//
+//  Created by Buka Cakrawala on 6/28/16.
+//  Copyright © 2016 Buka Cakrawala. All rights reserved.
+//
+
+import Foundation
