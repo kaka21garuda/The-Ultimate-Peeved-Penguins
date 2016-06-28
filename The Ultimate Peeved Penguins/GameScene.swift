e29b01258bf3a9cc7eca93155f747f5da9d62097
Buka Cakrawala <kaka10garuda@gmail.com>
2016-06-28 16:34:55 -0700

    Load Level 1 and Responsive obstacle:
    *Camera Node is Added
    *Ice Blocks are responsive
    *Levels

--- The Ultimate Peeved Penguins/GameScene.swift
+++ The Ultimate Peeved Penguins/GameScene.swift
@@ -12,10 +12,20 @@ class GameScene: SKScene {
     
     /* Game object connections */
     var catapultArm: SKSpriteNode!
+    var levelNode: SKNode!
+    
+    //camera helpers
+    var cameraTarget: SKNode!
     
     override func didMoveToView(view: SKView) {
         /* Set reference to catapultArm node */
         catapultArm = childNodeWithName("catapultArm") as! SKSpriteNode
+        levelNode = childNodeWithName("//levelNode")
+        
+        /* Load Level 1 */
+        let resourcePath = NSBundle.mainBundle().pathForResource("Level1", ofType: "sks")
+        let newLevel = SKReferenceNode (URL: NSURL (fileURLWithPath: resourcePath!))
+        levelNode.addChild(newLevel)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -37,10 +47,22 @@ class GameScene: SKScene {
         
         //Applying impulse to Penguin
         penguin.avatar.physicsBody?.applyImpulse(force)
+        
+        //ask the camera to follow penguins
+        cameraTarget = penguin.avatar
+        /*
+         Now that you have a target for the camera, you need to update the camera's position to follow the target.
+         */
     }
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        
+        //Checked wether we have a valid camera target to follow or not
+        if let cameraTarget = cameraTarget {
+            /* Set camera position to follow target horizontally, keep vertical locked */
+            camera?.position = CGPoint(x:cameraTarget.position.x, y:camera!.position.y)
+        }
     }
     
 }
